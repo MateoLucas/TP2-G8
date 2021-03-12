94fddcec61ad4a314d670df8e7b63eb7b3eeea73
Mateo Lucas <Mateo Lucas@DESKTOP-T9OM1K9>
2021-03-11 22:16:18 -0300

    borrador tool.c y tool.h

---
+++ tools.c
@@ -0,0 +1,52 @@
+#include "tools.h"
+#include <math.h>
+
+typedef struct  { 
+	double x; 
+	double y; 
+} Point ;		//cada punto tendra una estructura de este tipo
+
+typedef struct { 
+	Point InfIzq; 
+	Point SupDer; 
+} Rect;			//coordenadas de composicion de un rectangulo
+
+double getDistanceBetweenPoints(Point p1, Point p2)			//distancia euclidiana entre dos puntos
+{
+	//h^2 = c^2+c^2. ¿Habra que hacer alguna validacion?
+	return (sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)));
+}
+
+double getAngleBetweenPints(Point p1, Point p2)//angulo del segmento entre dos puntos.
+{
+	//siempre tomo a p1 como eje de rotación, al "norte"/eje cartesiano "y" como referencia del angulo
+	//angulo se mide en radianes (0 a 2pi)
+	return(   atan((p2.y - p1.y)/(p2.x - p1.x))   );
+}
+
+Point translatePoint(Point p, double distance, double angle)//translada el punto p la distancia y angulo especificados
+{
+	//x = r.cos(a)
+	//y = r.sin(a)
+	p.x += distance * cos(angle);
+	p.y += distance * sin(angle);
+	return p;
+}
+bool isPointEqual(Point p1, Point p2)//son iguales con error = 0.0000001
+{
+	if (getDistanceBetweenPoints(p1, p2) < 0.0000001)
+		return true;
+	else
+		return false;
+}
+double getRectArea(Rect r)//calcula el area de un rectangulo, solo puede recibir valores positivos de puntos
+{
+	return ((r.SupDer.x - r.InfIzq.x) * (r.SupDer.y - r.InfIzq.y));
+}
+bool isPointInRect(Point p, Rect r)//determina si el punto p esta en el rectangulo r
+{
+	if ((p.x <= r.SupDer.x && p.x >= r.InfIzq.x) && (p.y <= r.SupDer.y && p.y >= r.InfIzq.y))
+		return true;
+	else
+		return false;
+}
